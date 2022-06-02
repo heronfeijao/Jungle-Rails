3f409706c51127104638e4b1c3106f6f3a1c6677
Heron Feijao <heronfeijao@gmail.com>
2022-06-02 13:26:58 -0300

    added users and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  post "/logout" => "sessions#destroy"
+
   namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
+    get "categories/index"
+    get "categories/new"
+    get "categories/create"
   end
-  root to: 'products#index'
+  root to: "products#index"
 
   resources :about, only: [:index]
 
@@ -13,14 +19,14 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:create, :index, :new]
   end
