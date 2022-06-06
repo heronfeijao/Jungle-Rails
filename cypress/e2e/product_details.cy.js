1b33d3e137f43e0befc01ef3f7cc74b91b169b91
Heron Feijao <heronfeijao@gmail.com>
2022-06-06 20:37:10 -0300

    finished test to open product page

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,16 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Click on the product", () => {
+    cy.get(".products article")
+      .first()
+      .click();
+  });
+
+});
